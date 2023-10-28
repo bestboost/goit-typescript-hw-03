9b4f655ac1006815669ed0028168aa7b7b4b6303
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-10-28 19:30:30 +0300

    my cry

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,50 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person extends Key {
+  private key: number;
+  private tenants: string[] = [];
+
+  constructor(signature: number) {
+    super(signature);
+    this.key = signature;
+  }
+
+  public comeIn(person: string) {
+    this.tenants.push(person);
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+
+  public comeIn(person: string) {
+    if (person) {
+      this.door = true;
+    }
+  }
+
+  public abstract openDoor(): boolean;
+}
+
+class MyHouse extends House {
+  public openDoor() {
+    return true;
+  }
+}
+
+const randomNumber = Math.random();
+
+const key = new Key(randomNumber);
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
